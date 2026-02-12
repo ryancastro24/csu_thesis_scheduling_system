29cfad04649a650e61e2d8d614aeee51468d36a6
ryanjoel24 <ryanjoelimba@gmail.com>
2026-02-13 06:33:39 +0800

    final revision

--- src/lib/formsData.ts
+++ src/lib/formsData.ts
@@ -1,47 +1,41 @@
 export const forms = [
   {
     id: 1,
-    title: "Thesis Advisory Acceptance",
+    title: "Rev. 6_F-EVN-CCIS-002a THESIS ADVISORY ACCEPTANCE",
     link: "https://res.cloudinary.com/dmfgc0os2/raw/upload/v1758791166/ypinzd1kymlphh7xvrud.docx",
   },
   {
     id: 2,
-    title: "Application for Capstone",
+    title: "Rev. 6_F-EVN-CCIS-002b APPLICATION FOR CAPSTONE or THESIS DEFENSE",
     link: "https://res.cloudinary.com/dmfgc0os2/raw/upload/v1758791166/gymzancm4yep3rq1253m.docx",
   },
 
   {
     id: 3,
-    title: "CAPSTONE/THESIS ORAL EXAMINATION REPORT",
+    title: "Rev. 6_F-EVN-CCIS-002i ORAL EXAMINATION REPORT",
     link: "https://res.cloudinary.com/dmfgc0os2/raw/upload/v1770930183/irbnky3pxrmkgwaabseq.docx",
   },
 
   {
     id: 4,
-    title: "ORAL EXAMINATION SUMMARY SHEET ",
+    title: "Rev. 6_F-EVN-CCIS-002h ORAL EXAMINATION SUMMARY SHEET",
     link: "https://res.cloudinary.com/dmfgc0os2/raw/upload/v1770930393/xufs9lcymgkqfstysakq.docx",
   },
 
   {
     id: 5,
-    title: "ORAL EXAMINATION RATING SHEET",
+    title: "Rev. 6_F-EVN-CCIS-002g ORAL EXAMINATION RATING SHEET",
     link: "https://res.cloudinary.com/dmfgc0os2/raw/upload/v1770930501/no6ku2dc5hrnw6sipm0v.docx",
   },
   {
     id: 6,
-    title: "ORAL EXAMINATION REVISIONS MATRIX",
+    title: "ORev. 6_F-EVN-CCIS-002f ORAL EXAMINATION REVISIONS MATRIX",
     link: "https://res.cloudinary.com/dmfgc0os2/raw/upload/v1770930571/o006jg4ya3rsjvrrtxj2.docx",
   },
 
   {
     id: 7,
-    title: "CAPSTONE/THESIS ORAL EXAMINATION COMMENTS SHEET",
-    link: "https://res.cloudinary.com/dmfgc0os2/raw/upload/v1770930734/luxewisjwmhcbj5a0id5.docx",
-  },
-
-  {
-    id: 8,
-    title: "CAPSTONE/THESIS ORAL EXAMINATION COMMENTS SHEET",
+    title: "Rev. 6_F-EVN-CCIS-002e ORAL EXAMINATION COMMENTS SHEET",
     link: "https://res.cloudinary.com/dmfgc0os2/raw/upload/v1770930734/luxewisjwmhcbj5a0id5.docx",
   },
 ];
